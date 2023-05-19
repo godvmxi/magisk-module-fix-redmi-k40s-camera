35c49ccd81c35f5fa209dc6773a9359127bb6afc
godvmxi <godvmxi@gmail.com>
2023-05-19 23:08:19 +0800

    Use CN version to replace EEA and Global version

---
+++ customize.sh
@@ -0,0 +1,9 @@
+# shellcheck disable=SC2148,SC2086
+ui_print ""
+
+ui_print "For MIUI V14.0.3.0 EEA and Global ROM of Redmi K40S"
+
+ui_print ""
+ui_print "* Done"
+ui_print "  by Ball Virtual"
+ui_print " "
